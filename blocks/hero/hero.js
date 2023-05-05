3ab0be9ab7977b12e42fab2834ea379527dd70d0
Thomas Cantonnet <tcantonnet@adobe.com>
2023-05-05 15:40:02 +0200

    Fixes

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -3,9 +3,25 @@ export default function decorate(block) {
   container.classList.add('inner-container');
 
   const h1 = block.querySelector('h1');
-  const p = block.querySelectorAll('p');
+  const text = block.querySelectorAll('p:not([class*="button-container"])');
+  const link = block.querySelector('p[class*="button-container"]');
+  link.classList.remove('button-container');
 
-  container.append(h1, ...p);
+  // If block config includes "with-form", add search form
+  const searchForm = document.createElement('form');
+
+  if (block.classList.contains('with-search-form')) {
+    const input = document.createElement('input');
+    input.setAttribute('type', 'search');
+    input.setAttribute('name', 'query');
+    input.setAttribute('placeholder', 'Search by role or keyword');
+    input.setAttribute('aria-label', 'Search by role or keyword');
+    searchForm.append(input);
+
+    searchForm.setAttribute('display', 'visible');
+  }
+
+  container.append(h1, ...text, searchForm, link);
 
   block.appendChild(container);
 }
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -22,12 +22,10 @@ const LCP_BLOCKS = []; // add your LCP blocks to the list
 function buildHeroBlock(main) {
   const h1 = main.querySelector('h1');
   const picture = main.querySelector('picture');
-  const p = main.querySelectorAll('div[class=hero] p');
 
   // eslint-disable-next-line no-bitwise
   if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
     const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [picture, h1, ...p] }));
     main.prepend(section);
   }
 }
