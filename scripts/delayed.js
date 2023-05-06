e2ebe8c59f8ae43e7a3cdf63da84a6e55176ab37
Carsten Ziegeler <cziegeler@apache.org>
2023-05-06 14:36:17 +0200

    Only load video js if needed, fix control bar layout

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,16 +5,19 @@ import { sampleRUM, loadScript } from './lib-franklin.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
-loadScript('https://players.brightcove.net/1275282095001/9rGCgus7j_default/index.min.js', () => {
-  document.querySelectorAll('video').forEach((v) => {
-    const parent = v.parentElement;
-    // eslint-disable-next-line no-undef
-    const player = bc(v);
-    player.controls(true);
-    player.on('play', () => {
-      parent.children[0].style.visibility = 'hidden';
-      parent.children[1].style.display = 'block';
-      parent.children[1].children[0].style.display = 'block';
+const nodes = document.querySelectorAll('video');
+if (nodes.length > 0) {
+  loadScript('https://players.brightcove.net/1275282095001/9rGCgus7j_default/index.min.js', () => {
+    nodes.forEach((v) => {
+      const parent = v.parentElement;
+      // eslint-disable-next-line no-undef
+      const player = bc(v);
+      player.controls(true);
+      player.on('play', () => {
+        parent.children[0].style.visibility = 'hidden';
+        parent.children[1].style.display = 'block';
+        parent.children[1].children[0].style.display = 'block';
+      });
     });
   });
-});
+}
