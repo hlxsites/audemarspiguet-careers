2450be4ca20fba051b3e2fbbd7fbdb5e849d4e58
Hannes Hertach <hanneshertach@gmail.com>
2023-05-08 16:38:52 +0200

    mobile styling

--- blocks/jobs-list/jobs-list.js
+++ blocks/jobs-list/jobs-list.js
@@ -1,3 +1,5 @@
+import { decorateIcons } from '../../scripts/lib-franklin.js';
+
 export default function decorate(block) {
   block.textContent = '';
 
@@ -58,23 +60,41 @@ export default function decorate(block) {
   ];
 
   block.innerHTML = `
-    <div class="filters">
-      <div class="filter-head">Filters</div>
-      ${filters.map((filter, i) => `
-        <div>
-          <div class="filter-name">
-            ${filter.name}
-          </div>
-          <div class="filter-dropdown">
-            <button aria-controls="filter-${i}">${filter.values.length} ${filter.name}</button>
-            <div aria-hidden="true" id="filter-${i}" class="filter-dropdown-values">
-              ${filter.values.map((value) => `
-                ${value}
-              `).join('')}
+    <button aria-controls="filters-tab" name="filters toggle">
+      <span class="icon icon-filters"></span>Filters <span class="num-selected">(0 selected)</span>
+    </button>
+    <div aria-hidden="true" class="filters" id="filters-tab">
+      
+      <div class="filter-head">
+        <span class="icon icon-filters"></span>
+        <span>Filters</span>
+        <button name="filters close">
+          <span class="icon icon-close"></span>
+        </button>
+      </div>
+      <div class="filter-body">
+        ${filters.map((filter, i) => `
+          <div>
+            <label class="filter-name" for="dropdown-${filter.name}">
+              ${filter.name}
+            </label>
+            <div class="filter-dropdown" id="dropdown-${filter.name}">
+              <button aria-controls="filter-${i}">
+                ${filter.values.length} ${filter.name} 
+                <span class="icon icon-chevron-down"></span>
+              </button>
+              <div aria-hidden="true" id="filter-${i}" class="filter-dropdown-values">
+                ${filter.values.map((value) => `
+                  <div>
+                    <input id="check-${filter.name}-${value}" type="checkbox" />
+                    <label for="check-${filter.name}-${value}" ${value}>${value}</label>
+                  </div>
+                `).join('')}
+              </div>
             </div>
           </div>
-        </div>
-      `).join('')}
+        `).join('')}
+      </div>
     </div>
     <table>
       <thead>
@@ -95,4 +115,32 @@ export default function decorate(block) {
       </tbody>
     </table>
   `;
+
+  block.querySelector('button[name="filters close"]').addEventListener('click', () => {
+    block.querySelector('#filters-tab').setAttribute('aria-hidden', true);
+  });
+
+  block.querySelector('button[name="filters toggle"]').addEventListener('click', () => {
+    const filtersTab = block.querySelector('#filters-tab');
+    if (filtersTab.getAttribute('aria-hidden') === 'true') {
+      filtersTab.setAttribute('aria-hidden', false);
+    } else {
+      filtersTab.setAttribute('aria-hidden', true);
+    }
+  });
+
+  [...block.querySelectorAll('.filter-dropdown > button')].forEach((button) => {
+    button.addEventListener('click', () => {
+      const dropdownTab = block.querySelector(`#${button.getAttribute('aria-controls')}`);
+      if (dropdownTab.getAttribute('aria-hidden') === 'true') {
+        dropdownTab.setAttribute('aria-hidden', false);
+        dropdownTab.closest('.filter-dropdown').setAttribute('data-expanded', '');
+      } else {
+        dropdownTab.setAttribute('aria-hidden', true);
+        dropdownTab.closest('.filter-dropdown').removeAttribute('data-expanded');
+      }
+    });
+  });
+
+  decorateIcons(block);
 }
